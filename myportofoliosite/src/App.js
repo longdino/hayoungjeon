45dda3783a4f42145a4c9eb0664fd70894f17514
hayoungjeon <hayoungjeon@tamu.edu>
2020-03-09 18:31:13 -0500

    home link added

--- myportofoliosite/src/App.js
+++ myportofoliosite/src/App.js
@@ -9,15 +9,16 @@ function App() {
   return (
     <div className="demo-big-content">
       <Layout>
-        <Header className="header-color" title="Title" scroll>
+        <Header className="header-color" title="Hayoung Jeon" scroll>
           <Navigation>
+            <Link to="/">Home</Link>
             <Link to="/resume">Resume</Link>
             <Link to="/aboutme">About Me</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/contact">Contact</Link>
           </Navigation>
         </Header>
-        <Drawer title="Title">
+        <Drawer title="Menu">
           <Navigation>
             <Link to="/resume">Resume</Link>
             <Link to="/aboutme">About Me</Link>
