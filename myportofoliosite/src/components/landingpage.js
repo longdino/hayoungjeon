3f8e9e087d794ea0eec186b923b56bdafa66c8af
hayoungjeon <hayoungjeon@tamu.edu>
2020-03-09 16:05:05 -0500

    homepage

--- myportofoliosite/src/components/landingpage.js
+++ myportofoliosite/src/components/landingpage.js
@@ -1,10 +1,46 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/1024px-OOjs_UI_icon_userAvatar.svg.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+
+              <hr />
+              <p>
+                HTML/CSS | Bootstrap | JavaScript | React | React Native |
+                NodeJS | Express | MongoDB
+              </p>
+              <div className="social-links">
+                {/* LinkedIn */}
+                <a
+                  href="https://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+                {/* GitHub */}
+                <a
+                  href="https://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
