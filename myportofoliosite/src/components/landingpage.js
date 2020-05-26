fdfa4b75b93c5e13deb5b962e1c5006cdb25ecc6
hayoungjeon <hayung@gmail.com>
2020-05-26 13:33:38 -0500

    email icon added in homepage

--- myportofoliosite/src/components/landingpage.js
+++ myportofoliosite/src/components/landingpage.js
@@ -1,5 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Grid, Cell } from "react-mdl";
+import Media from "react-media";
+
+function Mailto({ email, subject, body, ...props }) {
+  return (
+    <a
+      href={`mailto:${email}?subject=${
+        encodeURIComponent(subject) || ""
+      }&body=${encodeURIComponent(body) || ""}`}
+    >
+      {props.children}
+    </a>
+  );
+}
 
 class Landing extends Component {
   render() {
@@ -13,6 +26,21 @@ class Landing extends Component {
               className="avatar-img"
             />
             <div className="banner-text">
+              {/* <Media
+                queries={{
+                  small: "(max-width: 599px)",
+                  medium: "(min-width: 600px) and (max-width: 1199px)",
+                  large: "(min-width: 1200px)",
+                }}
+              >
+                {(matches) => (
+                  <Fragment>
+                    {matches.small && <p>I am small!</p>}
+                    {matches.medium && <p>I am medium!</p>}
+                    {matches.large && <p>I am large!</p>}
+                  </Fragment>
+                )}
+              </Media> */}
               <h1>Software Developer</h1>
 
               <hr />
@@ -37,6 +65,14 @@ class Landing extends Component {
                 >
                   <i className="fa fa-github-square" aria-hidden="true" />
                 </a>
+                {/* Email */}
+                <Mailto
+                  email="hayung@gmail.com"
+                  subject="Help Needed"
+                  body="Hello Hayoung, "
+                >
+                  <i className="fa fa-envelope-o" aria-hidden="true" />
+                </Mailto>
               </div>
             </div>
           </Cell>
