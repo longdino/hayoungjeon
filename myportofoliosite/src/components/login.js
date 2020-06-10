37f3e9eefc86d5c9610e14a992be5a6b715bc4fa
hayoungjeon <hayung@gmail.com>
2020-06-09 19:19:27 -0500

    login page

--- myportofoliosite/src/components/login.js
+++ myportofoliosite/src/components/login.js
@@ -1,31 +1,50 @@
 import React, { useState } from "react";
-import { Button, Form, FormGroup, FormControl } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import { Button } from "react-bootstrap";
+import { Auth } from "aws-amplify";
 
 export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("hayung@gmail.com");
+  const [password, setPassword] = useState("1020");
 
-    function validateForm() {
-        return email.length > 0 && password.length > 0;
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      await Auth.signIn(email, password);
+      alert("Logged in");
+    } catch (e) {
+      alert(e.message);
     }
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-    }
-
-    return (
-        <div className="Login">
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="email" bsSize="large">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} />
-                </Form.Group>
-                <Form.Group controlId="password" bsSize="large">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control value={password} onChange={e => setPassword(e.target.value)} type="password" />
-                </Form.Group>
-                <Button block bsSize="large" disabled={!validateForm()} type="submit">Login</Button>
-            </Form>
-        </div>
-    );
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="email" bsSize="large">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="password" bsSize="large">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+        </Form.Group>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
 }
