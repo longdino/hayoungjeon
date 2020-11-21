a72ac87ce742a7c1870870ccd994378612bfe1f0
hayoungjeon <hayoungjeon@tamu.edu>
2020-11-20 23:06:10 -0600

    added initial proposal

--- src/components/csce641.js
+++ src/components/csce641.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 import { Grid, Cell } from "react-mdl";
+import PROS from "../static/Proposal_Jeon.pdf";
 import PROJ from "../static/Proposal.pdf";
 import UPDATE from "../static/Update.pdf";
 //import styled, { keyframes } from "styled-components";
@@ -11,8 +12,12 @@ class Csce extends Component {
                 <Grid className="contact-grid">
                     <Cell col={6}>
                         <h2>CSCE641</h2>
-                        <a href={PROJ} rel="noopener noreferrer" target="_blank">
+                        <a href={PROS} rel="noopener noreferrer" target="_blank">
                             Proposal
+                        </a>
+                        <br></br>
+                        <a href={PROJ} rel="noopener noreferrer" target="_blank">
+                            Proposal (Updated)
                         </a>
                         <br></br>
                         <a href={UPDATE} rel="noopener noreferrer" target="_blank">
